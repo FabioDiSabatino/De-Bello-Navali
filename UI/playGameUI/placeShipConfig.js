a09fab1eb5e5b5d83b3e46863688e3dae40abf9e
Fabio Di Sabatino <fabio_disabatino@hotmail.it>
2017-03-11 17:58:14 +0100

    -Started first pages: Place Ship. -Inserted four main content box.

---
+++ UI/playGameUI/placeShipConfig.js
@@ -0,0 +1,16 @@
+/**
+ * Created by fabiodisabatino on 11/03/17.
+ */
+(function () {
+    $(document).ready(function () {
+
+        $('.contentMain').matchHeight({
+            property: 'height',
+            target: $('.bodySize')
+
+        })
+    })
+
+
+
+}());
