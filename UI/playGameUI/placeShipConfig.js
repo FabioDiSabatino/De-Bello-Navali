247a2feee9c3e032c282fa731baec36a2c9ce8fb
Fabio Di Sabatino <fabio_disabatino@hotmail.it>
2017-03-14 18:17:11 +0100

    Place Ship
    -first version of flag banner
    -Create an img of Roman’s Fleet
    -Init ship template for the “ship list zone”

--- UI/playGameUI/placeShipConfig.js
+++ UI/playGameUI/placeShipConfig.js
@@ -1,7 +1,7 @@
 /**
  * Created by fabiodisabatino on 11/03/17.
  */
-(function () {
+(function (metaData) {
     $(document).ready(function () {
 
         $('.contentMain').matchHeight({
@@ -9,8 +9,31 @@
             target: $('.bodySize')
 
         })
+
+        setFlagBanner();
+
+
+
+
     })
 
 
 
 }());
+
+var setFlagBanner=function () {
+
+    $('.flagBanner').append("<img src="+metaData.flagBanner+" class=flagImg>");
+    $('.fleetName').text(metaData.fleet);
+}
+
+var setShipList= function () {
+    for(var i=0;i<metaData.numberShip;i++)
+    {   //riempire con i dati delle ship la relativa card, per ogni ship della flotta
+        $.get('shipCardTemplate.html')
+            .success(function(data) {
+                $('.listShip').html(data);
+            });
+    }
+
+}
---
+++ UI/utility/shipMetaData.js
@@ -0,0 +1,12 @@
+/**
+ * Created by fabiodisabatino on 12/03/17.
+ */
+
+metaData={
+    fleet:"Roman's Fleet",
+    flagBanner:"../playGameUI/img/romanflag.png",
+    numberShip:3,
+    ship2:"Bireme",
+    ship3:"Trireme",
+    ship4:"Quadrireme"
+}
