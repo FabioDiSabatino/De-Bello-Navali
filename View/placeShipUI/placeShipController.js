8db72a058fef5fc408e2f5998c183702313a36ac
Fabio Di Sabatino <fabio_disabatino@hotmail.it>
2017-04-08 18:39:36 +0200

    Some issues fixed
    issues fixed:
    -*When the draggable icon is moving left, "over" method is calling
    before "out" method. So the preview does not work well*
    -*There isn't any check for square not empty*
    -*.There isn't any check for the "Fleet Weight"*

---
+++ View/placeShipUI/placeShipController.js
@@ -0,0 +1,18 @@
+/**
+ * Created by fabiodisabatino on 08/04/17.
+ */
+var placeShipController={
+
+    data:{},
+
+    addShip:function (dim,position) {
+
+        console.log("ship"+dim+" in position"+position);
+    },
+
+
+
+
+
+
+}
