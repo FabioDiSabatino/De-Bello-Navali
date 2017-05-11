6f5933ae48441fa9d70846cd4713274bc9a91206
Montecchsilvia <montecchsilvia@gmail.com>
2017-05-11 10:46:52 +0200

    Create "module" Package under PlayGameUI Create metadata

---
+++ View/utility/GeneralShipMetaData.js
@@ -0,0 +1,23 @@
+/**
+ * Created by Silvia on 20/04/2017.
+ */
+
+metaData= {
+
+    Ship3: [{
+        ShipName:'Trireme',
+        Dimension:'3',
+        Img:'../placeShipUI/img/ship3.png',
+        Weight:'20',
+    }],
+
+    Ship4: [{
+        ShipName: 'Quadrireme',
+        Dimension:'4',
+        Img:'../placeShipUI/img/ship4.png',
+        Weight:'40'
+
+    }]
+
+}
+
---
+++ View/utility/ShipInstanceMetaData.js
@@ -0,0 +1,45 @@
+/**
+ * Created by Silvia on 20/04/2017.
+ */
+
+ShipData= {
+    Instance1: [{
+        ShipID: 1,
+        Dimension:3,
+        Position: [3, 5],
+        Orientation: 'Vertical',
+        Weapon: [{
+                Weapon1:[{
+                    WeaponName: "W1",
+                    TimeReload: 0,
+                    Ammo: 'infinity'/*lo lascio perchè decrementabile*/
+                }],
+                Weapon1:[{
+                    WeaponName: "W2",
+                    TimeReload: 2,
+                    Ammo:'3'/*lo lascio perchè decrementabile*/
+            }]
+            }],
+    }],
+
+    Instance2: [{
+        ShipID: 2,
+        Dimension:4,
+        Position: [2, 6],
+        Orientation: 'Orizontal',
+        Weapon: [{
+            Weapon1:[{
+                WeaponName: "W1",
+                TimeReload: 0,
+                Ammo: 'infinity'/*lo lascio perchè decrementabile*/
+            }],
+            Weapon5:[{
+                WeaponName: "W5",
+                TimeReload: 3,
+                Ammo:'2'/*lo lascio perchè decrementabile*/
+            }]
+        }],
+    }],
+
+}
+
